3dcf129470692e3994596ca4b06a4adc791d813b
Aliaksandr Kavalchuk <avkavalchuk@gmail.com>
2022-05-05 15:54:38 +0300

    lesson-1

---
+++ app/src/main/java/by/kavalchuk/aliaksandr/videoapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package by.kavalchuk.aliaksandr.videoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import by.kavalchuk.aliaksandr.videoapp.navigation.SetupNavHost
+import by.kavalchuk.aliaksandr.videoapp.screens.MainScreen
+import by.kavalchuk.aliaksandr.videoapp.ui.theme.VideoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            VideoAppTheme {
+                val navController = rememberNavController()
+                SetupNavHost(navController = navController)
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    VideoAppTheme {
+        MainScreen()
+    }
+}
